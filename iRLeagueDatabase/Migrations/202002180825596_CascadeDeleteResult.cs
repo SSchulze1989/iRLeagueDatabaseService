bab8b50d34f7534b69f4616e3b8b2e9441a8e2fa
Simon Schulze <simonschulze2589@hotmail.de>
2020-02-18 10:19:21 +0100

    Add project files.

---
+++ iRLeagueDatabase/Migrations/202002180825596_CascadeDeleteResult.cs
@@ -0,0 +1,20 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CascadeDeleteResult : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.ResultEntities", "ResultId", "dbo.SessionBaseEntities");
+            AddForeignKey("dbo.ResultEntities", "ResultId", "dbo.SessionBaseEntities", "SessionId", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ResultEntities", "ResultId", "dbo.SessionBaseEntities");
+            AddForeignKey("dbo.ResultEntities", "ResultId", "dbo.SessionBaseEntities", "SessionId");
+        }
+    }
+}
