27321773daccd5625d50d1d77fd0f0dfc80d47df
Simon Schulze <simonschulze2589@hotmail.de>
2020-07-31 11:59:21 +0200

    Added DbModelStore to DbConfiguration

---
+++ iRLeagueDatabase/LeagueDbConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Objects;
+using System.IO;
+
+namespace iRLeagueDatabase
+{
+    public class LeagueDbConfiguration : DbConfiguration
+    {
+        private readonly string DbModelStorePath = "C:\\iRLeagueDatabaseService\\DbModelStore";
+
+        public LeagueDbConfiguration() : base()
+        {
+            //var path = Path.GetDirectoryName(this.GetType().Assembly.Location);
+            var path = DbModelStorePath;
+            SetModelStore(new DefaultDbModelStore(path));
+        }
+    }
+}
