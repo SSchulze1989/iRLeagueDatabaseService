47ff67aa9811f963a82b03c8c68e9c5a2b6b4570
Simon Schulze <simonschulze2589@hotmail.de>
2020-09-22 00:33:02 +0200

    Change data type for Lap and Corner in Review to string

---
+++ iRLeagueDatabase/Migrations/202009180021301_AddLapCornerString.cs
@@ -0,0 +1,20 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddLapCornerString : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.IncidentReviewEntities", "OnLap", c => c.String());
+            AlterColumn("dbo.IncidentReviewEntities", "Corner", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.IncidentReviewEntities", "Corner", c => c.Int(nullable: false));
+            AlterColumn("dbo.IncidentReviewEntities", "OnLap", c => c.Int(nullable: false));
+        }
+    }
+}
