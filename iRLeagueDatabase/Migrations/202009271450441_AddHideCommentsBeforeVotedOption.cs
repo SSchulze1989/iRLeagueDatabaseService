6be7506118af61cb1e185c68c60faf32d0bc17bf
Simon Schulze <simonschulze2589@hotmail.de>
2020-09-27 19:13:32 +0200

    Add Hide comments before voting option

---
+++ iRLeagueDatabase/Migrations/202009271450441_AddHideCommentsBeforeVotedOption.cs
@@ -0,0 +1,18 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddHideCommentsBeforeVotedOption : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.SeasonEntities", "HideCommentsBeforeVoted", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.SeasonEntities", "HideCommentsBeforeVoted");
+        }
+    }
+}
