692c6df88fd7c1674ae1c27d425e6f773d405dc1
Simon Schulze <simonschulze2589@hotmail.de>
2020-11-08 21:00:38 +0100

    Add require calculation property to resultentity

---
+++ iRLeagueDatabase/Migrations/202011081954092_AddRequireCalculationOption.cs
@@ -0,0 +1,18 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRequireCalculationOption : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ResultEntities", "RequiresRecalculation", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ResultEntities", "RequiresRecalculation");
+        }
+    }
+}
