6eb35eaed792e2547f58e38dd928a479064667e1
Simon Schulze <simonschulze2589@hotmail.de>
2021-01-15 03:11:09 +0100

    Add league register and CheckUpdateController to check for last update in league database

---
+++ iRLeagueRESTService/Models/LeagueRegister.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace iRLeagueRESTService.Models
+{
+    [Serializable()]
+    public class LeagueRegister
+    {
+        private static string path = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/Leagues/Register.xml");
+        public List<LeagueEntry> Leagues { get; set; }
+
+        public LeagueRegister()
+        {
+            Leagues = new List<LeagueEntry>();
+        }
+
+        public static LeagueRegister Get()
+        {
+            var serializer = new XmlSerializer(typeof(LeagueRegister));
+
+            if (File.Exists(path) == false)
+            {
+                var newRegister = new LeagueRegister();
+                using (var stream = new StreamWriter(path))
+                {
+                    serializer.Serialize(stream, newRegister);
+                }
+            }
+
+            LeagueRegister register = null;
+            using (var stream = new StreamReader(path))
+            {
+                register = serializer.Deserialize(stream) as LeagueRegister;
+            }
+
+            return register;
+        }
+
+        public void Save()
+        {
+            var serializer = new XmlSerializer(typeof(LeagueRegister));
+
+            using (var stream = new StreamWriter(path))
+            {
+                serializer.Serialize(stream, this);
+            }
+        }
+    }
+}
