6a125672b9f57b76e74d402447bcf54eb46ca6ee
Simon Schulze <simonschulze2589@hotmail.de>
2021-02-17 13:20:09 +0100

    Fix - missing DataContract attributes

--- Communication/DataTransfer/Reviews/MemberPenaltySummaryDTO.cs
+++ Communication/DataTransfer/Reviews/MemberPenaltySummaryDTO.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace iRLeagueDatabase.DataTransfer.Reviews
 {
+    [DataContract]
     public class MemberPenaltySummaryDTO : BaseDTO
     {
+        [DataMember]
         public long MemberId { get; set; }
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public int Count { get; set; }
+        [DataMember]
         public int Points { get; set; }
+        [DataMember]
         public ReviewVoteDataDTO[] Penalties { get; set; }
     }
 }
--- Communication/DataTransfer/Reviews/ReviewsPenaltySummaryDTO.cs
+++ Communication/DataTransfer/Reviews/ReviewsPenaltySummaryDTO.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace iRLeagueDatabase.DataTransfer.Reviews
 {
+    [DataContract]
     public class ReviewsPenaltySummaryDTO : BaseDTO
     {
+        [DataMember]
         public int Count { get; set; }
+        [DataMember]
         public int Points { get; set; }
+        [DataMember]
         public MemberPenaltySummaryDTO[] DrvPenalties { get; set; }
     }
 }
