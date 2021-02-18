d9616ce8c24bb12334da39a27da4e9c789881d38
Simon Schulze <simonschulze2589@hotmail.de>
2021-02-18 08:52:41 +0100

    Fix - possible Null reference exception

--- iRLeagueRESTService/Data/ReviewDataProvider.cs
+++ iRLeagueRESTService/Data/ReviewDataProvider.cs
@@ -38,10 +38,11 @@ namespace iRLeagueRESTService.Data
             var modelDataProvider = new ModelDataProvider(DbContext);
             var reviews = modelDataProvider.GetReviews(reviewIds.ToArray());
             // get vote categories
-            var voteCatIds = reviews.SelectMany(x => x.AcceptedReviewVotes.Select(y => y.VoteCategoryId));
+            var voteCatIds = reviews.Where(x => x.AcceptedReviewVotes?.Count() > 0).SelectMany(x => x.AcceptedReviewVotes.Select(y => y.VoteCategoryId));
             var voteCats = DbContext.Set<VoteCategoryEntity>().Where(x => voteCatIds.Contains(x.CatId)).ToList().Select(x => mapper.MapToVoteCategoryDTO(x));
 
             var penalties = reviews
+                .Where(x => x.AcceptedReviewVotes?.Count() > 0)
                 .SelectMany(x => x.AcceptedReviewVotes)
                 .Where(x => x.CatPenalty > 0 && x.MemberAtFaultId != null);
             var driverPenalties = penalties
@@ -64,9 +65,9 @@ namespace iRLeagueRESTService.Data
             {
                 Reviews = reviews,
                 Total = reviews.Count(),
-                Open = reviews.Count(x => x.AcceptedReviewVotes.Count() == 0),
+                Open = reviews.Count(x => x.AcceptedReviewVotes == null || x.AcceptedReviewVotes.Count() == 0),
                 Voted = reviews.Count(x => x.Comments.Any(y => y.CommentReviewVotes.Count() > 0)),
-                Closed = reviews.Count(x => x.AcceptedReviewVotes.Count() > 0),
+                Closed = reviews.Count(x => x.AcceptedReviewVotes?.Count() > 0),
                 Penalties = penaltySummary,
                 //PenDrv = reviews
                 //    .SelectMany(x => x.AcceptedReviewVotes)
@@ -76,6 +77,7 @@ namespace iRLeagueRESTService.Data
                 //    .Count(),
                 //PenPts = reviews.Sum(x => x.AcceptedReviewVotes.Sum(y => y.CatPenalty)),
                 Results = reviews
+                    .Where(x => x.AcceptedReviewVotes?.Count() > 0)
                     .SelectMany(x => x.AcceptedReviewVotes)
                     .Where(x => x.VoteCategoryId != null)
                     .GroupBy(x => x.VoteCategoryId)
