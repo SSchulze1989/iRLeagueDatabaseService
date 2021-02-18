1e24949e7493f34e1dbf14c55418ad778b7c3917
Simon Schulze <simonschulze2589@hotmail.de>
2021-02-18 11:46:01 +0100

    Commentary

--- iRLeagueRESTService/Controllers/ReviewsController.cs
+++ iRLeagueRESTService/Controllers/ReviewsController.cs
@@ -1,5 +1,5 @@
 ﻿using iRLeagueDatabase;
-using iRLeagueDatabase.DataTransfer.Reviews;
+using iRLeagueDatabase.DataTransfer.Reviews.Convenience;
 using iRLeagueRESTService.Data;
 using iRLeagueRESTService.Filters;
 using log4net;
@@ -13,10 +13,28 @@ using System.Web.Http;
 
 namespace iRLeagueRESTService.Controllers
 {
+    /// <summary>
+    /// API endpoint to enable easy access to Reviews data for either a single session or a whole season
+    /// Data will span some simple summarized statistics and the complete reviews data including comments and votes
+    /// 
+    /// This endpoint only provides GET Method
+    /// </summary>
     public class ReviewsController : LeagueApiController
     {
+        /// <summary>
+        /// Logger for this class
+        /// </summary>
         private static readonly ILog logger = log4net.LogManager.GetLogger(typeof(ReviewsController));
 
+        /// <summary>
+        /// GET Method for reviews belonging to a single session
+        /// If no session id is provided the last finished session is used instead
+        /// </summary>
+        /// <param name="leagueName">Short name of the league</param>
+        /// <param name="sessionId">Id of the session (default: null)</param>
+        /// <param name="fields">Comma separated string to specify exact fields to return - JSON only! (default: null)</param>
+        /// <param name="excludeFields">If True, specified <paramref name="fields"/> will be excluded - JSON only! (default: false)</param>
+        /// <returns><see cref="ReviewsDTO"/> containing summary for the session reviews and penalties; <see cref="HttpError"/> on Error</returns>
         [HttpGet]
         [Authorize(Roles = LeagueRoles.UserOrAdmin)]
         public IHttpActionResult Get([FromUri] string leagueName, [FromUri] string sessionId = "0", [FromUri] string fields = null, bool excludeFields = false)
@@ -45,6 +63,7 @@ namespace iRLeagueRESTService.Controllers
 
                 var databaseName = GetDatabaseNameFromLeagueName(leagueName);
 
+                // Get reviews data from Data Access layer
                 ReviewsDTO data;
                 using (var dbContext = CreateDbContext(databaseName))
                 {
@@ -52,6 +71,7 @@ namespace iRLeagueRESTService.Controllers
                     data = reviewDataProvider.GetReviewsFromSession(sessionIdValue);
                 }
 
+                // return complete DTO or select fields
                 if (string.IsNullOrEmpty(fields))
                 {
                     return Ok(data);
