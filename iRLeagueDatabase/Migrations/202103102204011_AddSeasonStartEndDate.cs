a4a602a3d36a453cd14a553e6242ec09f8d2f955
Simon Schulze <simonschulze2589@hotmail.de>
2021-03-11 00:17:44 +0100

    Add SesonStart and SeasonEnd to database mapped values

---
+++ iRLeagueDatabase/Migrations/202103102204011_AddSeasonStartEndDate.cs
@@ -0,0 +1,20 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSeasonStartEndDate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.SeasonEntities", "SeasonStart", c => c.DateTime());
+            AddColumn("dbo.SeasonEntities", "SeasonEnd", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.SeasonEntities", "SeasonEnd");
+            DropColumn("dbo.SeasonEntities", "SeasonStart");
+        }
+    }
+}
