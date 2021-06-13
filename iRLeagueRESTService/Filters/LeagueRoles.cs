71e23777288d8622cc0d999a48413b9e6b455b30
Simon Schulze <simonschulze2589@hotmail.de>
2021-06-14 00:25:12 +0200

    Add authorization filter for league-role based authorization

--- iRLeagueRESTService/Filters/LeagueRoles.cs
+++ iRLeagueRESTService/Filters/LeagueRoles.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using iRLeagueDatabase.Enums;
+using iRLeagueDatabase.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +10,30 @@ namespace iRLeagueRESTService.Filters
 {
     public static class LeagueRoles
     {
-        public const string Admin = "Administrator";
-        public const string User = "User";
-        public const string UserOrAdmin = Admin + "," + User;
+        private static Dictionary<LeagueRoleEnum, string> RoleNames { get; }
+
+        static LeagueRoles()
+        {
+            // Get rolenames from enum
+            foreach (var role in Enum.GetValues(typeof(LeagueRoleEnum)).OfType<LeagueRoleEnum>())
+            {
+                var roleName = role.GetAttribute<LeagueRoleNameAttribute>()?.RoleName;
+                if (string.IsNullOrEmpty(roleName))
+                {
+                    roleName = Enum.GetName(typeof(LeagueRoleEnum), role);
+                }
+                RoleNames.Add(role, roleName);
+            }
+        }
+
+        public static string GetRoleName(LeagueRoleEnum role)
+        {
+            return role.GetAttribute<LeagueRoleNameAttribute>()?.RoleName ?? Enum.GetName(typeof(LeagueRoleEnum), role);
+        }
+
+        public static IDictionary<LeagueRoleEnum, string> GetAvailableRoles()
+        {
+            return new ReadOnlyDictionary<LeagueRoleEnum, string>(RoleNames);
+        }
     }
 }
