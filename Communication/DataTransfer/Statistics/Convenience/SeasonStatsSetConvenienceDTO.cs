19451728f5a9b0db6e4f3d7af5049435327ce9c9
Simon Schulze <simonschulze2589@hotmail.de>
2021-06-19 23:32:04 +0200

    Add convenience dtos for Statistics

---
+++ Communication/DataTransfer/Statistics/Convenience/SeasonStatsSetConvenienceDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.DataTransfer.Statistics.Convenience
+{
+    [DataContract]
+    public class SeasonStatsSetConvenienceDTO : StatisticConvenienceDTO
+    {
+        [DataMember]
+        public long SeasonId { get; set; }
+        [DataMember]
+        public long SeasonName { get; set; }
+    }
+}
---
+++ Communication/DataTransfer/Statistics/Convenience/StatisticConvenienceDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.DataTransfer.Statistics.Convenience
+{
+    [DataContract]
+    public class StatisticConvenienceDTO : BaseDTO
+    {
+        [DataMember]
+        public long LeagueId { get; set; }
+        [DataMember]
+        public string LeagueName { get; set; }
+
+        [DataMember]
+        public SeasonStatsSetConvenienceDTO[] SeasonStats { get; set; }
+        [DataMember]
+        public LeagueStatsSetConvenienceDTO[] LeagueStats { get; set; }
+    }
+}
