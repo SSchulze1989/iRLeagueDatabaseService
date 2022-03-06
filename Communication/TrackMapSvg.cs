ebe3a2efec399f872d8d6d8c0764449abdd80b81
Simon Schulze <simonschulze2589@hotmail.de>
2022-03-06 22:30:29 +0100

    add trackmap controller

---
+++ Communication/TrackMapSvg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueManager.Locations
+{
+    [DataContract]
+    public class TrackMapSvg
+    {
+        [DataMember]
+        public string TrackBackgroundSvg { get; set; }
+        [DataMember]
+        public string TrackInactiveSvg { get; set; }
+        [DataMember]
+        public string TrackActiveSvg { get; set; }
+        [DataMember]
+        public string TrackPitRoadSvg { get; set; }
+        [DataMember]
+        public string TrackStartFinishSvg { get; set; }
+        [DataMember]
+        public string TrackTurnsSvg { get; set; }
+    }
+}
