3b4938514b85539a94b02d1c2bfcf1a954419303
Simon Schulze <simonschulze2589@hotmail.de>
2022-04-16 09:29:41 +0200

    Add filters to scoring tables

---
+++ iRLeagueDatabase/Entities/Filters/StandingsFilterOptionEntity.cs
@@ -0,0 +1,30 @@
+﻿using iRLeagueDatabase.Entities.Results;
+using iRLeagueManager.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.Entities.Filters
+{
+    public class StandingsFilterOptionEntity : Revision
+    {
+        [Key, Column(Order = 0)]
+        public long ResultsFilterId { get; set; }
+
+        [ForeignKey(nameof(ScoringTable)), Column(Order = 1)]
+        public long ScoringTableId { get; set; }
+
+        public virtual ScoringTableEntity ScoringTable { get; set; }
+        public string ResultsFilterType { get; set; }
+        public string ColumnPropertyName { get; set; }
+        public ComparatorTypeEnum Comparator { get; set; }
+        public bool Exclude { get; set; }
+        public string FilterValues { get; set; }
+
+        public override object MappingId => ResultsFilterId;
+    }
+}
---
+++ iRLeagueDatabase/Migrations/202204160648278_AddStandingsFilters.cs
@@ -0,0 +1,42 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStandingsFilters : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.StandingsFilterOptionEntities",
+                c => new
+                    {
+                        ResultsFilterId = c.Long(nullable: false, identity: true),
+                        ScoringTableId = c.Long(nullable: false),
+                        ResultsFilterType = c.String(),
+                        ColumnPropertyName = c.String(),
+                        Comparator = c.Int(nullable: false),
+                        Exclude = c.Boolean(nullable: false),
+                        FilterValues = c.String(),
+                        CreatedOn = c.DateTime(),
+                        LastModifiedOn = c.DateTime(),
+                        Version = c.Int(nullable: false),
+                        CreatedByUserId = c.String(),
+                        CreatedByUserName = c.String(),
+                        LastModifiedByUserId = c.String(),
+                        LastModifiedByUserName = c.String(),
+                    })
+                .PrimaryKey(t => t.ResultsFilterId)
+                .ForeignKey("dbo.ScoringTableEntities", t => t.ScoringTableId, cascadeDelete: true)
+                .Index(t => t.ScoringTableId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.StandingsFilterOptionEntities", "ScoringTableId", "dbo.ScoringTableEntities");
+            DropIndex("dbo.StandingsFilterOptionEntities", new[] { "ScoringTableId" });
+            DropTable("dbo.StandingsFilterOptionEntities");
+        }
+    }
+}
