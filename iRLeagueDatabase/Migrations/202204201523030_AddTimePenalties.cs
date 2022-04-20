dfa2c1fbad1e11495c2369f37355e092571fcd18
Simon Schulze <simonschulze2589@hotmail.de>
2022-04-20 17:25:06 +0200

    Add migration

---
+++ iRLeagueDatabase/Migrations/202204201523030_AddTimePenalties.cs
@@ -0,0 +1,22 @@
+﻿namespace iRLeagueDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTimePenalties : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ScoredResultRowEntities", "PenaltyTime", c => c.Long(nullable: false));
+            AddColumn("dbo.AddPenaltyEntities", "PenaltyTime", c => c.Long(nullable: false));
+            AddColumn("dbo.ReviewPenaltyEntities", "PenaltyTime", c => c.Long(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ReviewPenaltyEntities", "PenaltyTime");
+            DropColumn("dbo.AddPenaltyEntities", "PenaltyTime");
+            DropColumn("dbo.ScoredResultRowEntities", "PenaltyTime");
+        }
+    }
+}
